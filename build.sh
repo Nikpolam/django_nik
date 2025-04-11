51025186e4c6e67911f4ec58f24251f2f0735278
Nikpolam <125681419+Nikpolam@users.noreply.github.com>
2025-04-11 20:00:17 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# exit on error
+
+# Установка Python зависимостей
+pip install -r requirements.txt
+
+# Очистка старых статических файлов
+python manage.py collectstatic --no-input --clear
+
+# Сборка фронтенда
+cd frontend
+npm install
+npm run build
+cd ..
+
+# Копирование собранных файлов фронтенда
+mkdir -p staticfiles
+cp -r frontend/dist/* staticfiles/
+
+# Сборка бэкенда
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Создание суперпользователя
+python create_superuser.py 
